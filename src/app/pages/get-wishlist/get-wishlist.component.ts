c2ac65d7ee4a13a3056be29c4baf4355b7816ec6
nikhilkumbhare <nikhilk190697@gmail.com>
2021-06-15 13:42:49 +0530

    Added: Get Wishlist UI and Api

--- src/app/pages/get-wishlist/get-wishlist.component.ts
+++ src/app/pages/get-wishlist/get-wishlist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+import { UserService } from 'src/app/service/userService/user.service';
 
 @Component({
   selector: 'app-get-wishlist',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GetWishlistComponent implements OnInit {
 
-  constructor() { }
+  constructor(private user : UserService, private snackBar: MatSnackBar) { }
+  wishlistArray = [] as any;
 
-  ngOnInit(): void {
+  openSnackBar(message: string, duration: number) {
+    let config = new MatSnackBarConfig();
+    if (duration != 0) {
+      config.duration = duration;
+    }
+    this.snackBar.open(message, undefined, config);
   }
 
+  ngOnInit(): void {
+    this.displayItems();
+  }
+
+  displayItems() {
+    let arr = [] as any
+    this.user.getWishlistItem().subscribe((res) => {
+      console.log(res)
+      arr = res
+      this.wishlistArray = arr.result
+      console.log(this.wishlistArray);
+    }, (error) => {
+      console.log(error)
+    })
+  }
+
+  deleteBook(data: any) {
+    console.log(data);
+    let bookId = data.product_id._id;
+    console.log(bookId);
+    this.user.deleteWishlistItem(bookId).subscribe((res: any) => {
+      console.log(res)
+      this.openSnackBar(res.message, 2000);
+      this.displayItems()
+    }, (error) => {
+      console.log(error)
+    })
+  }
 }
