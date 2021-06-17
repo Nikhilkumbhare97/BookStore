090cbfe7ce6ed2bc5210ec08ecbae0cd9244f2ed
nikhilkumbhare <nikhilk190697@gmail.com>
2021-06-17 11:00:29 +0530

    Refactor: Search Book In DashBoard

--- src/app/pipe/search.pipe.ts
+++ src/app/pipe/search.pipe.ts
@@ -5,20 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchPipe implements PipeTransform {
 
-  transform(value: any, searchText: string) {
-   if (value.length == 0 || searchText == ''){
-      return value;
-   }
+  transform(items: any[], searchText: string): any[] {
+    if (!items) return [];
+    if (!searchText) return items;
 
-   const books = [];
-   for (const book of value) {
-     console.log(book)
-     if (book['bookName'] === searchText) {
-       books.push(book);
-     }
-   }
-
-   return books;
-
+    searchText = searchText.toLowerCase();
+    return items.filter(it => {
+      console.log(it['bookName'])
+      return it['bookName'].toLowerCase().includes(searchText);
+    });
   }
 }
